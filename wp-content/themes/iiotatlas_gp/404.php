27e038afbba3f7385ad788055a230b4c66a76414
Ajar PC <asbel.elmec@gmail.com>
2021-02-10 15:09:36 -0400

    ADD scss

---
+++ wp-content/themes/iiotatlas_gp/404.php
@@ -0,0 +1,1 @@
+<h1>404 vaya no funciona</h1>
--- wp-content/themes/iiotatlas_gp/functions.php
+++ wp-content/themes/iiotatlas_gp/functions.php
@@ -1,6 +1,10 @@
 <?php
 /**
  * Funciones y definiciones.
+ * @package Iiot Atlas Sample
+ * @author  Asbel Apaza
+ * @license null
+ * @link    https://www.iiotatlas.com/
  *
  * Para aniadir codigo personalizado PHP.
  * Solo edita este archivo si tienes accesso directo en el servidor  (to fix errors if they happen).
